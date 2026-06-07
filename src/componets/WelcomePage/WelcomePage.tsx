ed7adc9fbe6ab37f670caa04f8d1e607968b1abe
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-06-07 08:10:46 +0100

    modified page

--- src/componets/WelcomePage/WelcomePage.tsx
+++ src/componets/WelcomePage/WelcomePage.tsx
@@ -111,8 +111,9 @@ function WelcomePage() {
     const randomIndex = Math.floor(Math.random() * 20);
     let index = randomIndex;
     let length = mockedEmails.length;
+    console.log(length);
     const interval = setInterval(() => {
-      if (index + 1 !== length) {
+      if (index !== length || index < length) {
         const paidEmailMassage = `${mockedEmails[index]} just grabbed the DMG`;
         toast.success(paidEmailMassage);
         index++;
